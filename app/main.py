e0711b72e5eba12bd71b52e8b5276e7950846e3c
Juan Ovalle <jpovalleg@gmail.com>
2025-02-27 18:05:30 -0500

    Added prometheus

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.controllers import auth, userController
 from app.db.database import init_db
@@ -14,9 +15,16 @@ app.add_middleware(
     allow_headers=[""],                     # Permitir todos los encabezados
 )
 
+instrumentator = Instrumentator(should_group_status_codes=False).instrument(app)
+instrumentator.expose(app, endpoint="/metrics")
+
 @app.on_event("startup")
 async def on_startup():
     await init_db()
 
 app.include_router(auth.router, prefix="", tags=["auth"])
 app.include_router(userController.router, prefix="/user", tags=["update user"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the User API"}
